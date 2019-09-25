9cd70fb4f5e893395a5e69ba725ee42bdf0c3bb3
s094392 <s094392@gmail.com>
2019-09-25 22:28:45 +0800

    first commit

---
+++ app.py
@@ -0,0 +1,47 @@
+from flask import Flask
+import ast
+import json
+
+app = Flask(__name__)
+
+def classname(cls):
+    return cls.__class__.__name__
+
+def jsonify_ast(node, level=0):
+    fields = {}
+    for k in node._fields:
+        fields[k] = '...'
+        v = getattr(node, k)
+        if isinstance(v, ast.AST):
+            if v._fields:
+                fields[k] = jsonify_ast(v)
+            else:
+                fields[k] = classname(v)
+
+        elif isinstance(v, list):
+            fields[k] = []
+            for e in v:
+                fields[k].append(jsonify_ast(e))
+
+        elif isinstance(v, str) or isinstance(v, int) or isinstance(v, float):
+            fields[k] = v
+
+        elif v is None:
+            fields[k] = None
+
+        else:
+            fields[k] = 'unrecognized'
+
+    ret = { classname(node): fields }
+    return ret
+
+@app.route('/')
+def ast2json():
+    code = '''a + 3
+a + a + 3'''
+    tree = ast.parse(code)
+    data = jsonify_ast(tree)
+    return data
+
+if __name__ == '__main__':
+    app.run()
