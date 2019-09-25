531c0fdec18fe4c54eb8fa563cb3312bf39cd5d1
CodyTseng <cody880528@hotmail.com>
2019-09-26 01:08:44 +0800

    Frontend

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useRef, useEffect, useState } from 'react';
+import debounce from 'lodash/debounce';
 import './App.css';
+import './vtree.css';
+
+const visualize = debounce((code, vt) => {
+  fetch("http://dorm.muller.nctu.me:5000/",
+    { body: code, method: 'POST' })
+    .then(res => res.json())
+    .then(res => {
+      if (!vt) return;
+      vt.mode(vt.MODE_PYTHON_AST)
+        .data(res)
+        .conf('showArrayNode', false)
+        .update();
+    });
+}, 250);
+
 
 function App() {
+  const ref = useRef(null);
+  const [vt, setVt] = useState();
+  const [code, setCode] = useState("");
+
+  useEffect(() => {
+    if (!ref.current) return;
+    setVt(window.vtree(ref.current));
+  }, [ref])
+
+  const handleCodeChange = e => {
+    setCode(e.target.value);
+    visualize(e.target.value, vt);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <textarea className="TextArea" value={code} onChange={handleCodeChange} />
+      <div className="Graph" ref={ref} />
     </div>
   );
 }
